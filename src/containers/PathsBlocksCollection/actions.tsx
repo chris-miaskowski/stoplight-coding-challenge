cbb94075cdda3c24948daec5959f39c6c9ad2394
Chris Miaskowski <chris.miaskowski@tenfold.com>
2018-09-07 17:30:52 +0200

    Fixed missing param names

--- src/containers/PathsBlocksCollection/actions.tsx
+++ src/containers/PathsBlocksCollection/actions.tsx
@@ -1,12 +1,18 @@
+import { validate } from "swagger-parser";
+
 export const LOAD_SPEC = "LOAD_SPEC";
 
 export const loadSpec = () => (dispatch: Function) => {
   chrome.storage.local.get("spec", ({ spec }) => {
-    if (spec) {
-      dispatch({
-        type: LOAD_SPEC,
-        spec
-      });
-    }
+    validate(spec)
+      .then(api => {
+        if (spec) {
+          dispatch({
+            type: LOAD_SPEC,
+            spec
+          });
+        }
+      })
+      .catch();
   });
 };
